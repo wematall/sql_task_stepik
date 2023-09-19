a5a499b6cbd83fed9b31583d9620c5d6e514d8b8
Andrey <wematall@gmail.com>
2023-09-19 20:44:03 +0300

    added data to tabel book

--- 1.Model_basics/insert_to_book.sql
+++ 1.Model_basics/insert_to_book.sql
@@ -1,2 +1,11 @@
 INSERT INTO book (title, author, price, amount)
-VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 3);
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Белая гвардия', 'Булгаков М.А.', 540.50, 5);
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Идиот', 'Достоевский Ф.М.', 460.00, 10);
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
