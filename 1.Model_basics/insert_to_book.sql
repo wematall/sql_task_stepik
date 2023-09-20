f1f9c623ee6299519eaf84fa081796c5ad8a10db
Andrey <wematall@gmail.com>
2023-09-20 11:57:18 +0300

    added data insertion

--- 1.Model_basics/insert_to_book.sql
+++ 1.Model_basics/insert_to_book.sql
@@ -9,3 +9,6 @@ VALUES ('Идиот', 'Достоевский Ф.М.', 460.00, 10);
 
 INSERT INTO book (title, author, price, amount)
 VALUES ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Стихотворения и поэмы', 'Есенин С.А.', 650.00, 15);
