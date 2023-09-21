e9a7dbb84f0038ea4174b0d55d6b84828cf92271
Andrey <wematall@gmail.com>
2023-09-21 11:41:19 +0300

    simple example conditions where or and > <

--- 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
+++ 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
@@ -52,5 +52,11 @@ SELECT author, title, price
 FROM book
 WHERE amount < 10;
 
+SELECT title, author, price
+FROM book
+WHERE author = 'Булгаков М.А.' AND price > 600;
 
+SELECT title, author, price
+FROM book
+WHERE (author = 'Булгаков М.А.' OR author = 'Есенин С.А.') AND price > 600;
 
