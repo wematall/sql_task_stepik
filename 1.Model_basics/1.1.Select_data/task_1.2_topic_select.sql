560cfbc395b10270d32c9fb2c2f64b69a677b42e
Andrey <wematall@gmail.com>
2023-09-21 09:50:19 +0300

    считаем ндс

--- 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
+++ 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
@@ -11,3 +11,10 @@ FROM book;
 SELECT title, amount,
     amount * 1.65 AS pack
 FROM book;
+
+SELECT title,
+    price,
+    ROUND((price*18/100)/(1+18/100),2) AS tax,
+    ROUND(price/(1+18/100),2) AS price_tax
+FROM book;
+
