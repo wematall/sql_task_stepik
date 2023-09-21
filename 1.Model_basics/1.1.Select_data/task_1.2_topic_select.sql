8d942ea96a77716590f8d9f319c38336eb0fb34b
Andrey <wematall@gmail.com>
2023-09-21 10:27:06 +0300

    считаем скидку

--- 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
+++ 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
@@ -30,5 +30,12 @@ SELECT title, amount, price,
     ROUND(IF(amount < 4, price*0.5, price*0.7), 2) AS sale
 FROM book;
 
+SELECT title, amount, price,
+    ROUND(IF(amount<4, price*0.5, IF(amount<11, price*0.7, price*0.9)),2) AS sale,
+    IF(amount<4, 'скидка 50%', IF(amount<11, 'скидка 30%', 'скидка 10%')) AS Ваша_скидка
+FROM book;
+
+
+
 
 
