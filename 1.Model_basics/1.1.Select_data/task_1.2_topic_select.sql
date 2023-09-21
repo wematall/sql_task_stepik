43f3ca5b2e8bcbeaae0115c8f02015ea8d383e58
Andrey <wematall@gmail.com>
2023-09-21 09:44:54 +0300

    new column counted

--- 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
+++ 1.Model_basics/1.1.Select_data/task_1.2_topic_select.sql
@@ -3,3 +3,11 @@ SELECT * FROM book;
 SELECT author, title, price FROM book;
 
 SELECT title AS Название, author AS Автор FROM book;
+
+SELECT title, author, price, amount,
+    price * amount AS total
+FROM book;
+
+SELECT title, amount,
+    amount * 1.65 AS pack
+FROM book;
